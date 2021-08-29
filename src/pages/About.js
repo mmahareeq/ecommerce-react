baf8aec6d3a0cb328483407715ab99dde5949778
mmahareeq <52622730+mmahareeq@users.noreply.github.com>
2021-08-29 14:41:05 +0300

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,63 @@
 import React,{useContext} from 'react'
-
+import PageHero from '../components/PageHero'
+import styled from 'styled-components'
 export default function About() {
     
     return (
-        <div>
-            <h1>Hello about</h1>
+      <main> 
+            <div>
+                <PageHero tittle='About'/>
+                <Wrapper className='page section section-center'>
+                    <img src='' alt='nice desk' />
+                    <article>
+                    <div className='title'>
+                        <h2>our story</h2>
+                        <div className='underline'></div>
+                    </div>
+                    <p>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat
+                        accusantium sapiente tempora sed dolore esse deserunt eaque
+                        excepturi, delectus error accusamus vel eligendi, omnis beatae.
+                        Quisquam, dicta. Eos quod quisquam esse recusandae vitae neque
+                        dolore, obcaecati incidunt sequi blanditiis est exercitationem
+                        molestiae delectus saepe odio eligendi modi porro eaque in libero
+                        minus unde sapiente consectetur architecto. Ullam rerum, nemo iste
+                        ex, eaque perspiciatis nisi, eum totam velit saepe sed quos
+                        similique amet. Ex, voluptate accusamus nesciunt totam vitae esse
+                        iste.
+                    </p>
+                    </article>
+                </Wrapper>
         </div>
+    </main>
+        
     )
 }
+const Wrapper = styled.section`
+display: grid;
+  gap: 4rem;
+  img {
+    width: 100%;
+    display: block;
+    border-radius: var(--radius);
+    height: 500px;
+    object-fit: cover;
+  }
+  p {
+    line-height: 2;
+    max-width: 45em;
+    margin: 0 auto;
+    margin-top: 2rem;
+    color: var(--clr-grey-5);
+  }
+  .title {
+    text-align: left;
+  }
+  .underline {
+    margin-left: 0;
+  }
+  @media (min-width: 992px) {
+    grid-template-columns: 1fr 1fr;
+  }
+`
+
