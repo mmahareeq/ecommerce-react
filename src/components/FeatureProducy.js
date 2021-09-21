7b61dc8d2b0a13243e52383e6fa0d333f2716c50
mmahareeq <mmjaryq@gmail.com>
2021-09-21 15:10:57 +0300

    build app

--- src/components/FeatureProducy.js
+++ src/components/FeatureProducy.js
@@ -1,6 +1,7 @@
 import React,{useContext} from 'react'
 import {ProductContext}  from '../utils/ProductContext.js'
 import styled from 'styled-components';
+import {formatPrice} from '../utils/helper'
 export default function FeatureProducy(props) {
     const Productss = useContext(ProductContext);
 
@@ -14,7 +15,7 @@ export default function FeatureProducy(props) {
     }
     //const Img = Productss.Product[0].image;
     return (
-        <div>
+        <Wrapper>
             <h2>Feature Products</h2>
             <hr width="100px" />
             <div className="featur-product">
@@ -27,15 +28,20 @@ export default function FeatureProducy(props) {
                
                         <Frame>
                             <span className="namee">{Name}</span>
-                            <span style={{color:'#ab7a5f'}}>{Price}</span>
+                            <span style={{color:'#ab7a5f'}}>{formatPrice(Price)}</span>
                         </Frame></div>
                         )
                 })}
             </div> 
             <Buttons>ALL PRODUCTS</Buttons>
-        </div>
+        </Wrapper>
     )
 }
+const Wrapper = styled.div`
+display:flex;
+flex-direction:column;
+text-align:center;
+justify-content:center`;
 const Frame = styled.div`
 display:flex;
 flex-direction:row;
@@ -49,10 +55,11 @@ const Buttons = styled.button`
   height: 45px;
   
   background-color: #ab7a5f;
-  margin:1.5rem;
+  margin-left:35rem;
   border:none;
   font-size: 1.2rem;
-  border-radius: 0.2rem;`;
+  border-radius: 0.2rem;
+  `;
 /* <img src={Productss.Product[0].image} alt="" width="350px" height="225px"/>
                 <Frame>
                     <span>{Productss.Product[0].name}</span>
